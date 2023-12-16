2b88792d1dbb76e7c83dae4ac6a803645bc0e077
dusty-turner <dusty.s.turner@gmail.com>
2023-12-15 21:40:14 -0600

    have it ordering and modeling properly now, big win!

--- 03-eda/ggtheme_field.R
+++ 03-eda/ggtheme_field.R
@@ -4,3 +4,17 @@ theme_field <- theme(panel.background = element_rect(fill = "white",
                      panel.grid.major = element_line(linewidth = 0.5, linetype = 'solid',
                                                      colour = "black"),
                      panel.grid.major.y = element_blank())
+
+grab <- function(x){
+  x[,,1] 
+}
+
+plot_matrix <- function(mat) {
+  par(mar = rep(0, 4))
+  standardize <- function(x) (x - min(x)) / (max(x) - min(x)) 
+  mat |>
+    standardize() |>
+    as.raster() |>
+    plot(interpolate = FALSE) # ?graphics::plot.raster
+}
+
